6892935023ecb42c0a148bb3d90d1e256bdbbd8a
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-05-08 22:34:39 +0530

    Create start.py

---
+++ lazyleech/plugins/start.py
@@ -0,0 +1,24 @@
+# lazyleech - Telegram bot primarily to leech from torrents and upload to Telegram
+# Copyright (c) 2021 lazyleech developers <theblankx protonmail com, meliodas_bot protonmail com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from pyrogram import Client, filters
+
+@Client.on_message(filters.command('start'))
+async def start_cmd(client, message):
+    await message.reply_text('''<b>Hi, I am Torrent Lazyleech.</b>
+I can leech direct / torrent links.
+For More Info /help
+
+Source Code - https://github.com/WeebTime/Torrent-Bot-Lazyleech'''.strip())
