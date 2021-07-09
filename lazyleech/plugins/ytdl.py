c7c03616088b831e1b0b063d57edbf602386ad57
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-07-09 13:04:04 +0530

    Small Fix

--- lazyleech/plugins/ytdl.py
+++ lazyleech/plugins/ytdl.py
@@ -59,7 +59,7 @@ BASE_YT_URL = "https://www.youtube.com/watch?v="
 YOUTUBE_REGEX = comp_regex(
     r"(?:youtube\.com|youtu\.be)/(?:[\w-]+\?v=|embed/|v/|shorts/)?([\w-]{11})"
 )
-PATH = "./ytdl/ytsearch.json"
+PATH = "./lazyleech/ytdl/ytsearch.json"
 DOWN_PATH = "./lazyleech/utils/ytdl/downloads"
 
 
