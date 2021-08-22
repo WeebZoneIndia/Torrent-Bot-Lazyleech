f6359035aa1e62239e0a41726fc6da093b0c1500
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-08-22 13:26:02 +0530

    Progress for Rename

--- lazyleech/plugins/rename.py
+++ lazyleech/plugins/rename.py
@@ -15,12 +15,13 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
+import re
+import math
 import time
 import html
 import asyncio
 from .. import ALL_CHATS
 from pyrogram import Client, filters
-from ..utils.upload_worker import progress_callback_data
 
 @Client.on_message(filters.command('rename') & filters.chat(ALL_CHATS))
 async def rename(client, message):
@@ -43,10 +44,50 @@ async def rename(client, message):
         return
     msg = await message.reply_text('Added to Queue')
     filepath = os.path.join(str(message.from_user.id), name)
-    await msg.edit_text('<code>Downloading...</code>')
-    await download_message.download(file_name=filepath, progress=progress_callback_data, progress_args=(msg, 'Downloading', True))
+    await download_message.download(file_name=filepath, progress=progress_for_pyrogram, progress_args=('Downloading...' msg, c_time)
     await asyncio.sleep(5)
-    await msg.edit_text('<code>Uploading...</code>')
-    await message.reply_document(filepath, caption=name, progress=progress_callback_data, progress_args=(msg, 'Uploading', False))
+    await message.reply_document(filepath, caption=name, progress=progress_for_pyrogram, progress_args=('Uploading...' msg, c_time))
     await msg.delete()
     os.remove(filepath)
+
+    async def progress_for_pyrogram(current, total, ud_type, message, start):
+    PROGRESS = """
+    • {0} of {1}
+    • Speed: {2}/s
+    • ETA: {3}"""
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+        progress = "[{0}{1}] \n".format(
+            ''.join(["█" for i in range(math.floor(percentage / 5))]),
+            ''.join([" " for i in range(10 - math.floor(percentage / 5))]))
+        tmp = progress + PROGRESS.format(
+            humanbytes(current),
+            humanbytes(total),
+            humanbytes(speed),
+            estimated_total_time if estimated_total_time != '' else "0 s")
+        try:
+            await message.edit_text(
+                text="{}\n\n {}".format(ud_type, tmp), parse_mode='markdown')
+        except:
+            pass
+
+def humanbytes(size):
+    # https://stackoverflow.com/a/49361727/4723940
+    # 2**10 = 1024
+    if not size:
+        return ""
+    power = 2 ** 10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
