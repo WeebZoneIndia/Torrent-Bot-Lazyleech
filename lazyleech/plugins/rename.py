09e83a67f4675f0a91ebefcfbb867dd629aff4bb
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-08-22 13:37:55 +0530

    Update rename.py

--- lazyleech/plugins/rename.py
+++ lazyleech/plugins/rename.py
@@ -46,62 +46,11 @@ async def rename(client, message):
     if download_message is None:
         await message.reply_text('Media required')
         return
-    msg = await message.reply_text('Added to Queue')
+    msg = await message.reply_text('<code>Downloading</code>')
     filepath = os.path.join(str(message.from_user.id), name)
-    await download_message.download(file_name=filepath, progress=progress_for_pyrogram, progress_args=('Downloading...' msg, c_time)
+    await download_message.download(filepath)
     await asyncio.sleep(5)
-    await message.reply_document(filepath, caption=name, progress=progress_for_pyrogram, progress_args=('Uploading...' msg, c_time))
+    await msg.edit_text('<code>Uploading</code>')
+    await message.reply_document(filepath, caption=name)
     await msg.delete()
     os.remove(filepath)
-
-    async def progress_for_pyrogram(current, total, ud_type, message, start):
-    PROGRESS = """
-    • {0} of {1}
-    • Speed: {2}/s
-    • ETA: {3}"""
-    now = time.time()
-    diff = now - start
-    if round(diff % 10.00) == 0 or current == total:
-        percentage = current * 100 / total
-        speed = current / diff
-        elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
-
-        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
-
-        progress = "[{0}{1}] \n".format(
-            ''.join(["█" for i in range(math.floor(percentage / 5))]),
-            ''.join([" " for i in range(10 - math.floor(percentage / 5))])
-        )
-
-        tmp = progress + PROGRESS.format(
-            humanbytes(current),
-            humanbytes(total),
-            humanbytes(speed),
-            estimated_total_time if estimated_total_time != '' else "0 s"
-        )
-        try:
-            await message.edit_text(
-                text="{}\n\n {}".format(
-                    ud_type,
-                    tmp
-                ),
-                parse_mode='markdown'
-            )
-        except:
-            pass
-
-def humanbytes(size):
-    # https://stackoverflow.com/a/49361727/4723940
-    # 2**10 = 1024
-    if not size:
-        return ""
-    power = 2 ** 10
-    n = 0
-    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
-    while size > power:
-        size /= power
-        n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
