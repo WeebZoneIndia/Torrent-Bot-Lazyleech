e008336a7a4b3ed38087840258d34adc27d0e969
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-08-27 23:13:33 +0530

    Update rename.py

--- lazyleech/plugins/rename.py
+++ lazyleech/plugins/rename.py
@@ -19,23 +19,21 @@ import html
 import math
 import os
 import re
-import time
 
 from pyrogram import Client, filters
 
-from .. import ALL_CHATS, ForceDocumentFlag, SendAsZipFlag, PROGRESS_UPDATE_DELAY
+from .. import ALL_CHATS, help_dict, ForceDocumentFlag, SendAsZipFlag, PROGRESS_UPDATE_DELAY
 from ..utils.upload_worker import _upload_file
 
 
 @Client.on_message(filters.command(['rename', 'filerename']) & filters.chat(ALL_CHATS))
 async def rename(client, message):
-    text = (message.text or message.caption).split(None, 1)
+    text = message.command
     command = text.pop(0).lower()
     if 'file' in command:
         flags = (ForceDocumentFlag,)
     else:
         flags = ()
-    c_time = time.time()
     name = message.text.split(None, 1)[1]
     available_media = ("audio", "document", "photo", "sticker", "animation", "video", "voice", "video_note")
     download_message = None
@@ -53,11 +51,11 @@ async def rename(client, message):
     if download_message is None:
         await message.reply_text('Media required')
         return
-    filepath = os.path.join(str(message.from_user.id), name)
+    filepath = os.path.join(name)
     msg = await message.reply_text('Downloading...')
     await download_message.download(filepath)
     await msg.edit_text("Uploading...")
     await asyncio.sleep(PROGRESS_UPDATE_DELAY)
-    await _upload_file(client, message, msg, name, filepath, ForceDocumentFlag in flags)
-    await msg.edit_text("Renamed Files Successfully")
+    await _upload_file(client, message, msg, name, filepath, flags)
+    await msg.edit_text("File renamed")
     os.remove(filepath)
